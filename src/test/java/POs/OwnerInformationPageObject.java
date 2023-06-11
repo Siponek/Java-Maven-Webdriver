55e902761403a8948d9452e380094fcef145fd4d
Siponek <szinkowicz@gmail.com>
2023-06-12 00:58:06 +0200

    Added: NewPet and NewVisit links. Added first visit text

--- src/test/java/POs/OwnerInformationPageObject.java
+++ src/test/java/POs/OwnerInformationPageObject.java
@@ -12,6 +12,9 @@ public class OwnerInformationPageObject extends BasePagePO{
     @FindBy(xpath = "/html/body/div/div/table[1]/tbody/tr[3]/td")
     public WebElement ownerCityName;
 
+    @FindBy(xpath = "//*[text() = 'the first visit']")
+    public WebElement tfFirstVisit;
+
     public OwnerInformationPageObject(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
@@ -21,6 +24,13 @@ public class OwnerInformationPageObject extends BasePagePO{
         click(By.linkText("Edit Owner"));
     }
 
+    public void goToNewPetPage() {
+        click(By.linkText("Add New Pet"));
+    }
+
+    public void goToNewVisitPage() {
+        click(By.linkText("Add Visit"));
+    }
     public String getOwnerCity() {
         return ownerCityName.getText();
     }
