859327374682301b1f7aef44f29524f006036f88
Siponek <szinkowicz@gmail.com>
2023-06-11 19:35:59 +0200

    Added page object for OwnerPage

---
+++ src/test/java/POs/FindOwnersPageObject.java
@@ -0,0 +1,21 @@
+package POs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+//page:url = http://localhost:8080/owners/find.html
+public class FindOwnersPageObject extends BasePagePO {
+
+    //Xpath returns a single element, so you can use it to find a single element on the page. In this case it is the button with
+    //the text "Add Owner". You can use the By.xpath() method to find the element.
+    private By addOwnerButton = By.xpath("//a[@href='/owners/new']");
+
+    public FindOwnersPageObject(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToAddOwnerPage() {
+        click(addOwnerButton);
+    }
+}
+
