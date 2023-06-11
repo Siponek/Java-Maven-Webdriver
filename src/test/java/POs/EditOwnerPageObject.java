5bc8d8bc3775c86e8cb274ef8697c6f814df8809
Siponek <szinkowicz@gmail.com>
2023-06-11 21:51:06 +0200

    Added Scenario: Try to edit an owner with empty name TEST

--- src/test/java/POs/EditOwnerPageObject.java
+++ src/test/java/POs/EditOwnerPageObject.java
@@ -3,20 +3,37 @@ package POs;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import static POs.BasePagePO.ANSI_BLUE;
+import static POs.BasePagePO.ANSI_RESET;
 
 import java.time.Duration;
 
 public class EditOwnerPageObject extends BasePagePO {
+//    @FindBy(xpath = "//*[text() = 'must not be empty']")
+//    public WebElement spanMustNotEmpty;
+
+    @FindBy(css = "span.help-inline")
+    public WebElement spanMustNotEmpty;
+
     public EditOwnerPageObject(WebDriver driver) {
         super(driver);
     }
+
+    
 
     public void  fillElement(By by, String text) {
         WebElement element = find(by);
         element.clear();
         element.sendKeys(text);
+    }
+    public boolean isErrorDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(spanMustNotEmpty));
+        System.out.println(ANSI_BLUE + "spanMustNotEmpty.getText() = " + spanMustNotEmpty.getText() + ANSI_RESET);
+        return spanMustNotEmpty.getText().contains("must not be empty") || spanMustNotEmpty.getText().contains("nie może być puste");
     }
     public void editOwner(String firstName, String lastName, String address, String city, String telephone) {
         fillElement(By.id("firstName"), firstName);
--- src/test/java/test/EditOwnerTest.java
+++ src/test/java/test/EditOwnerTest.java
@@ -17,23 +17,19 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.Duration;
 
+import static POs.BasePagePO.ANSI_BLUE;
+import static POs.BasePagePO.ANSI_RESET;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EditOwnerTest {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+
     private WebDriver driver;
     private FindOwnersPageObject findOwnersPage;
     private OwnerInformationPageObject ownerInfoPage;
     private EditOwnerPageObject editOwnerPage;
+
+
 
     @BeforeEach
     public void setUp() {
@@ -65,5 +61,16 @@ public class EditOwnerTest {
         // Then verify that the owner's city has been updated
         System.out.println(ANSI_BLUE + ownerInfoPage.getOwnerCity() + ANSI_RESET);
         assertEquals("New York", ownerInfoPage.getOwnerCity());
+    }
+    
+//    Scenario: Try to edit an owner with empty name
+    @Test
+    public void testEditOwnerWithEmptyName() {
+        findOwnersPage.searchForAllOwners();
+        findOwnersPage.clickOnOwner("George Franklin");
+        ownerInfoPage.goToEditOwnerPage();
+        editOwnerPage.editOwner("", "Franklin", "110 W. Liberty St.", "New York", "6085551023");
+        editOwnerPage.updateOwner();
+        assertTrue(editOwnerPage.isErrorDisplayed());
     }
 }
